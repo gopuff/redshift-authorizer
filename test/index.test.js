18be91f1e3ce9696def183f05f66bb8732119fba
Bobby Kozora <bobby@kozora.me>
2018-06-26 14:29:04 -0400

    integration test ensuring we can query Redshift

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,19 @@
 const RedshiftAuthorizer = require('../dist/redshiftAuthorizer')
 const prefix = process.env.NODE_ENV || 'production'
 
-const rsAuth = RedshiftAuthorizer.getDbConnection(`/${prefix}/redshift`)
-  .then(rsAuth => {
-    let res = typeof rsAuth.connect === 'function'
-      ? 'Passed!'
-      : 'Failed!'
+const db = RedshiftAuthorizer.getDbConnection(`/${prefix}/redshift`)
+  .then(db => {
+    let res = typeof db.connect === 'function'
+      ? 'Passed!' : 'Failed!'
 
+      db.any('SELECT * FROM users LIMIT 1', [true])
+        .then(function (data) {
+          // Success
+          console.log('data successfully retrieved')
+        })
+        .catch(function(err) {
+          // Uh oh...
+          console.error(err)
+        })
     console.log(res)
   })
